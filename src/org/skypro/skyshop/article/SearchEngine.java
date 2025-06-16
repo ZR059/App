f09da0422fad9ee030716d51cd852d40d61ea40b
ZR059 <zubairov_90@mail.ru>
2025-06-16 13:35:13 +0500

    сделано дз #20

--- src/org/skypro/skyshop/article/SearchEngine.java
+++ src/org/skypro/skyshop/article/SearchEngine.java
@@ -15,7 +15,7 @@ public class SearchEngine {
         Comparator<Searchable> comparator = (o1, o2) -> {
             int lengthCompare = Integer.compare(o2.getName().length(), o1.getName().length());
             if (lengthCompare != 0) {
-                return lengthCompare;
+                return lengthCompare;//
             }
             return o1.getName().compareTo(o2.getName());
         };
